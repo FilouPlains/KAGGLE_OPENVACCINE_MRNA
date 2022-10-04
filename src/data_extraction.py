369fc786b2bf71edf5f2b91963950104d2c9b60c
FilouPlains <lucas.rouaud@gmail.com>
2022-10-04 15:37:27 +0200

    Adding folder and first programs.

---
+++ src/data_extraction.py
@@ -0,0 +1,128 @@
+"""Cleaning the `.json` file into a `.npy` file. Like so, data are much
+easier to use.
+
+To load created data, do like so : `data = np.load("data/test.npy")`
+"""
+
+__authors__ = ["BEL Alexis", "BELAKTIB Anas", "OUSSAREN Mohamed",
+               "ROUAUD Lucas"]
+__contact__ = ["alexbel28@yahoo.fr", "anas.belaktib@etu.u-paris.fr",
+               "oussarenmohamed@outlook.fr", "lucas.rouaud@gmail.com"]
+__date__ = "23/09/2022"
+__version__ = "1.0.0"
+__copyright__ = "CC BY-SA"
+
+PATH = "../../DATA/"
+
+from numpy import array, row_stack, save
+
+
+def __into_array(lst_str):
+    """Function which convert a line like `"['1.0', '2.0']"` into numpy array.
+
+    Parameters
+    ----------
+    lst_str : str
+        The string to convert.
+
+    Returns
+    -------
+    numpy.array
+        The numpy array resulting from the string conversion.
+    """
+    # Take the line, split all `,"`, split then all `,` to do a list.
+    # Then convert all `str` into `float` with map then reconvert it to list
+    # then numpy.Array.
+    return array(list(map(float, lst_str.split(",\"")[0][1:-1].split(","))))
+
+
+def write_test():
+    """Function which writes the testing dataset.
+    """
+    with open(PATH + "/test.json", "r", encoding="utf-8") as file:
+        data = None
+
+        for line in file:
+            # Splitting line to work on theme.
+            line_lst = line.strip().split(":")
+
+            # Getting sequence informations.
+            # Getting datat by splitting on `,`.
+            seq_id = line_lst[2].split(",")[0][1:-1]
+            sequence = line_lst[3].split(",")[0][1:-1]
+            structure = line_lst[4].split(",")[0][1:-1]
+            loop_type = line_lst[5].split(",")[0][1:-1]
+            noise = float(line_lst[6].split(",")[0])
+            score = float(line_lst[7].split(",")[0][:-1])
+
+            # Concatenate data.
+            line_clean: array = array([seq_id, sequence, structure, loop_type,
+                                       noise, score],
+                                      dtype=object)
+
+            # Initialize data or stack to the array.
+            if data is None:
+                data = line_clean
+            else:
+                data = row_stack([data, line_clean])
+
+    # Saving all data into a `.npy` file.
+    save(PATH + "/test.npy", data)
+    print("[[DONE: WRITING AND SAVING TEST DATASET]]")
+
+
+def write_train():
+    """Function which writes the training dataset.
+    """
+    with open(PATH + "/train.json", "r", encoding="utf-8") as file:
+        data = None
+
+        for line in file:
+            # Splitting line to work on theme.
+            line_lst = line.strip().split(":")
+
+            # Getting sequence informations.
+            # Getting datat by splitting on `,`.
+            seq_id = line_lst[2].split(",")[0][1:-1]
+            sequence = line_lst[3].split(",")[0][1:-1]
+            structure = line_lst[4].split(",")[0][1:-1]
+            loop_type = line_lst[5].split(",")[0][1:-1]
+            noise = float(line_lst[6].split(",")[0])
+            sn_filter = float(line_lst[7].split(",")[0])
+            score = int(line_lst[9].split(",")[0])
+
+            # Getting data to predict.
+            # ERRORS.
+            E_reactivity = __into_array(line_lst[10])
+            E_mg_ph10 = __into_array(line_lst[11])
+            E_ph10 = __into_array(line_lst[12])
+            E_mg_c50 = __into_array(line_lst[13])
+            E_c50 = __into_array(line_lst[14])
+            # VALUES.
+            reactivity = __into_array(line_lst[15])
+            mg_ph10 = __into_array(line_lst[16])
+            ph10 = __into_array(line_lst[17])
+            mg_c50 = __into_array(line_lst[18])
+            c50 = __into_array(line_lst[19][:-1])
+
+            # Concatenate data.
+            line_clean: array = array([seq_id, sequence, structure, loop_type,
+                                       noise, sn_filter, score, reactivity,
+                                       E_reactivity, mg_ph10, E_mg_ph10, ph10,
+                                       E_ph10, mg_c50, E_mg_c50, c50, E_c50],
+                                      dtype=object)
+
+            # Initialize data or stack to the array.
+            if data is None:
+                data = line_clean
+            else:
+                data = row_stack([data, line_clean])
+
+    # Saving all data into a `.npy` file.
+    save(PATH + "/training.npy", data)
+    print("[[DONE: WRITING AND SAVING TRAINING DATASET]]")
+
+
+if __name__ == "__main__":
+    write_test()
+    write_train()
---
+++ src/embedding.py
@@ -0,0 +1,127 @@
+"""Create en embedding matrix with given `string` data.
+"""
+
+__authors__ = ["BEL Alexis", "BELAKTIB Anas", "OUSSAREN Mohamed",
+               "ROUAUD Lucas"]
+__contact__ = ["alexbel28@yahoo.fr", "anas.belaktib@etu.u-paris.fr",
+               "oussarenmohamed@outlook.fr", "lucas.rouaud@gmail.com"]
+__date__ = "23/09/2022"
+__version__ = "1.0.0"
+__copyright__ = "CC BY-SA"
+
+import numpy as np
+
+BASE = {
+    "A": 0,
+    "U": 1,
+    "G": 2,
+    "C": 3
+}
+
+PAIRED = {
+    "(": 0,
+    ".": 1,
+    ")": 2
+}
+
+LOOP = {
+    "E": 0,
+    "B": 1,
+    "M": 2,
+    "X": 3,
+    "I": 4,
+    "H": 5,
+    "S": 6
+}
+
+
+def hot_encoding(array: np.array(str), dic: dict[str, int]) \
+        -> np.array(np.array(int)):
+    """Modify a numpy array of string letters into a one with string digits.
+
+    Parameters
+    ----------
+    array : np.array(str)
+        The array to modify.
+    dic : dict[str, str]
+        Dictionary containing which characters to modify (`key`) with what
+        (`value`).
+
+    Returns
+    -------
+    np.array(np.array(int))
+        The modify array.
+    """
+    # Break string into list, with 1 character per index.
+    brk_array: list[list[str]] = [*array]
+    int_array: list[list[int]] = []
+
+    # Parsing all array's row.
+    for row in brk_array:
+        trlt_base: list[int] = []
+
+        # Parsing all row's base.
+        for base in row:
+            trlt_base.append(dic[base])
+
+        int_array.append(trlt_base)
+
+    # Returning into a numpy array.
+    return np.array(int_array)
+
+
+def positional_embedding(length: int, seq_len: int) \
+        -> np.array(np.array(float)):
+    """Return positional embedding of a given length (`row`) and seq_len
+    (`column`).
+
+    Parameters
+    ----------
+    length : int
+        Number of row in the embedding
+    seq_len : int
+        The length of the sequence.
+
+    Returns
+    -------
+    np.array(np.array(float))
+        The positional embedding for given data.
+    """
+    # Create an empty array.
+    array: np.array = np.zeros([length, seq_len])
+    # Create a array of `seq_len` size.
+    to_filter: np.array = np.array(range(seq_len))
+    # Create a array of `length` size.
+    dimension: np.array = np.transpose(np.array([range(length)] * seq_len))
+
+    # To filter odd and even index.
+    o_f: np.array = to_filter % 2 == 0
+    e_f: np.array = to_filter % 2 == 1
+
+    # Doing arithmetic operation applied with a vectoriel
+    array[:, o_f] = np.divide(dimension[:, o_f], 10_000 ** (2 * to_filter[o_f]
+                                                            / seq_len))
+    array[:, e_f] = np.divide(dimension[:, e_f], 10_000 ** (2 * to_filter[e_f]
+                                                            / seq_len))
+
+    return array
+
+
+if __name__ == '__main__':
+    # Data importation.
+    data_train: np.array = np.load(
+        "../../DATA/training.npy", allow_pickle=True)
+
+    # Creating input embedding matrix.
+    sequence: np.array = hot_encoding(array=data_train[:, 1], dic=BASE)
+    second_strct: np.array = hot_encoding(array=data_train[:, 2], dic=PAIRED)
+    loop_type: np.array = hot_encoding(array=data_train[:, 3], dic=LOOP)
+
+    # Creating positional embedding matrix.
+    dim: tuple = sequence.shape
+    pos: np.array = positional_embedding(length=dim[0], seq_len=dim[1])
+
+    # Creating the embedding matrix.
+    emb_seq: np.array = sequence + pos
+    emb_sec: np.array = second_strct + pos
+    emb_loop: np.array = loop_type + pos
