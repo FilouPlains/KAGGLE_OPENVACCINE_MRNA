a061bf7f066048ef33a24c58bec7a2f6f70b0677
“abelaktib” <belaktib.anas@gmail.com>
2022-10-23 21:52:48 +0200

    Inception neural network configuration

---
+++ src/neural_network/inception.py
@@ -0,0 +1,81 @@
+
+from keras import Input, Model
+from keras.layers import Conv1D, Flatten, Dense, concatenate, MaxPooling1D,Multiply,Add,Bidirectional,GRU,LSTM
+from keras.utils.vis_utils import plot_model
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# import input_network.own_embedding as oweb
+def __mcrmse(y_true, y_pred):
+    """[PRIVATE]Loss function
+
+    Parameters
+    ----------
+    y_true : float
+        The actual `y`.
+    y_pred : float
+        The `y` to predict.
+
+    Returns
+    -------
+    rmse
+        The compute rmse.
+    """
+    mse = tf.reduce_mean(tf.square(y_true - y_pred), axis=1)
+
+    return tf.reduce_mean(tf.sqrt(mse), axis=1)
+
+def __inception(inputs):
+    # 1st
+    conv_1 = Conv1D(22, (1), padding='same', activation='relu')(inputs)
+    conv_1 = Conv1D(22, (3), padding='same', activation='relu')(conv_1)
+    # 2
+    conv_2 = Conv1D(22, (1), padding='same', activation='relu')(inputs)
+    conv_2 = Conv1D(22, (5), padding='same', activation='relu')(conv_2)
+    # 3
+    conv_3 = MaxPooling1D((3), strides=(1), padding='same')(inputs)
+    conv_3 = Conv1D(22, (1), padding='same', activation="relu")(conv_3)
+    # 4
+    conv_4 = Conv1D(22, (1), padding='same', activation='relu')(inputs)
+    conc_1 = concatenate([conv_1, conv_2, conv_3, conv_4], axis=2)
+
+    return conc_1
+
+
+def my_model_inception(inputs, original, mask):
+    n_inception = 2
+    print("Simple inception network with %d modules" % (n_inception))
+    # inputs = Input(shape=(130, 1))
+
+    # layer
+    inception_i = inputs
+    for _ in range(n_inception):
+        inception_i = __inception(inception_i)
+
+#   flat_1 = Flatten()(inception_i)
+    dense_1 = Dense(1200, activation='relu')(inception_i)  # (flat_1)
+    dense_1 = Bidirectional(GRU(256, dropout=0.2, return_sequences=True, kernel_initializer='orthogonal'))(dense_1)
+    dense_1 = Bidirectional(GRU(256, dropout=0.2, return_sequences=True, kernel_initializer='orthogonal'))(dense_1)
+    dense_1 = Bidirectional(GRU(256, dropout=0.2, return_sequences=True, kernel_initializer='orthogonal'))(dense_1)
+    dense_1 = Bidirectional(GRU(256, dropout=0.2, return_sequences=True, kernel_initializer='orthogonal'))(dense_1)
+    dense_2 = Dense(600, activation='relu')(dense_1)
+    dense_3 = Dense(150, activation='relu')(dense_2)
+    dense_4 = Dense(5, activation='relu')(dense_3)
+    
+    masked = Multiply()([dense_4, mask])
+    output = Dense(5, activation='linear')(masked)
+
+
+    # Set the model.
+    model = Model(inputs=original + [mask], outputs=output)
+
+    # Compile then return the model.
+    model.compile(optimizer="adam", loss=__mcrmse)
+    print(model.summary())
+    return model
+
+if __name__ == "__main__":
+    inputs_3 = Input(shape=(130, 120))
+    original_3 = inputs_3
+
+    my_model_inception(inputs_3, [original_3], Input((130, 5)))
