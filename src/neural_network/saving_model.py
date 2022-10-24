566c20cb2a8289fe26fec310c69a413ac42ef108
Ale6 <alexbel28@yahoo.fr>
2022-10-24 10:32:08 +0200

    Loading the model that only predict

--- src/neural_network/saving_model.py
+++ src/neural_network/saving_model.py
@@ -1,20 +1,9 @@
 """Save and load the fitted model.
 """
 
-# [I]
-import input_network.own_embedding as oweb
-# [N]
-import numpy as np
-# [O]
-import os
-
 # [K]
 from keras import Input
-# [N]
-from neural_network.cnn import cnn
-from neural_network.mcrmse import mcrmse
-# [T]
-from tensorflow.keras.models import model_from_json
+from keras.models import load_model
 
 __authors__ = ["BEL Alexis", "BELAKTIB Anas", "OUSSAREN Mohamed",
                "ROUAUD Lucas"]
@@ -34,14 +23,8 @@ def saving_model(model, output_file):
     output_file : 
         Name of the folder which will contain the model
     """
-    # Save the weight.
-    model.save_weights(output_file)
-
-    # Save the architecture of the model with the custom layer.
-    model_json = model.to_json()
-
-    with open(output_file[:-3] + ".json", "w", encoding="utf-8") as json_file:
-        json_file.write(model_json)
+    # Save the model. Must be a h5 file.
+    model.save(output_file)
 
     print("=" * 80 + "\n")
     print(f"Saved model as {output_file}.\n")
@@ -57,35 +40,12 @@ def loading_model(model_file):
     Return :
         Fitted model
     """
-    # Load the architecture of the model.
-    with open(model_file, "r", encoding="utf-8") as json_file:
-        json_savedModel = json_file.read()
 
-    loaded_model = model_from_json(json_savedModel)
-
-    # Create tha path to the weight file.
-    weight_file = model_file[:-5] + ".h5"
-
-    # Load weights into new model.
-    loaded_model.load_weights(weight_file)
-    loaded_model.compile(optimizer="adam", loss=mcrmse)
+    # Load model only use for the prediction.
+    model = load_model(model_file, compile = False)
 
     print("=" * 80 + "\n")
-    print(f"Model {model_file} and weight {weight_file} loaded.\n")
+    print(f"Model {model_file} loaded.\n")
     print("=" * 80 + "\n")
 
-    return loaded_model
-
-
-if __name__ == "__main__":
-    # Output for the 3 model
-    inputs_3 = Input(shape=(130, 120))
-    original_3 = inputs_3
-
-    # Creating model 3
-    model3 = cnn(inputs_3, [original_3], Input((130, 5)))
-    # Saving model
-    saving_model(model3, "../data/model_test")
-    # loading model
-    model2 = loading_model("../data/model_test.json")
-    print(model2.summary())
+    return model
