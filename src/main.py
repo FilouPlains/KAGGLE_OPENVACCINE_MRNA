Correcting a masking error for RNABERT embedding in 'main.py'.

src/main.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
                 gap = 130 - len(line)
 
                 data_rewrite[i] = np.array(dataset[i] + empty_list * gap)
-            
+
             dataset = data_rewrite
         else:
             for i, line in enumerate(dataset):
@@ -129,13 +129,12 @@ if __name__ == "__main__":
     elif arg["rnabert_embedding"]:
         inputs = Input(shape=(130, 120))
         original = [inputs]
-        data_input = dataset
+        data_input = mask.format_input(dataset, n_line, 120)
 
     # Training a neural network.
     if arg["predict_data"] is None:
         model, history = cv.cross_val(arg["cnn"], inputs, original, data_input,
                                       masked, data_output, file_out)
-
     # Predict `Y`.
     else:
         model = save.loading_model(arg["input"])