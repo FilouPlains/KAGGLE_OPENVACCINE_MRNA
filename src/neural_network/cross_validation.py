9c829ca7bb4c4e888622e5360248d0afdc100ef7
Ale6prog <91945856+Ale6prog@users.noreply.github.com>
2022-10-26 14:36:09 +0200

    Save the best model

--- src/neural_network/cross_validation.py
+++ src/neural_network/cross_validation.py
@@ -1,5 +1,5 @@
 """Calculate the mcmre of train and validation.
-"""
+""" 
 import numpy as np
 
 import input_network.embedding as emb
@@ -14,9 +14,10 @@ from keras import Input
 
 import neural_network.cnn as cnn
 import neural_network.inception as inc
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 
-EPOCHS = 500
+EPOCHS = 100
 BATCH_SIZE = 50
 
 
@@ -34,20 +35,22 @@ def cross_val(is_nn_cnn, inputs, original, data_input, mask, data_output,
         A vector of masking
     data_output :
         Data input that will be given to the model
-    name_file :
-        Name of the file for saving the figure
-
+    file_out :
+        Name of the file for saving the figure and the model.
     """
     if is_nn_cnn:
         model = cnn.cnn(inputs, original, Input((130, 5)))
     else:
         model = inc.inception(inputs, original, Input((130, 5)))
 
-    print(model.summary())
+    earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+    mcp_save = ModelCheckpoint(file_out , save_best_only=True, monitor='val_loss', mode='min')
+    #reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=0, epsilon=1e-4, mode='min')
 
     # Fitting the model
     history = model.fit([data_input, mask], data_output, validation_split=0.2,
-                        epochs=EPOCHS, batch_size=BATCH_SIZE)
+                        epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks= [earlyStopping,
+                        mcp_save])
 
     print("\n" + "=" * 80 + "\n")
     print("Writting `data/history.csv` to access to 'LOSS' and 'VAL_LOSS'.\n")
